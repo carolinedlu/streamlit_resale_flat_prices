81ad343b54206a151635b32e643fcb9638d4ee21
Greco92 <rcws1992@gmail.com>
2021-09-09 17:34:21 +0800

    version_1

---
+++ streamlit_resale_flat_prices_app.py
@@ -0,0 +1,38 @@
+# imports
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+
+# title of app
+st.title('Resale Flat Prices')
+
+# read data
+data = pd.read_csv('flat_prices_full.csv')
+
+
+
+
+# display data
+st.write(data)
+
+# set plot and figure size
+fig, ax = plt.subplots(figsize=(15,10))
+# plot ax
+ax = sns.boxplot(x='resale_price', y='flat_type', data=data, order=sorted(list(data['flat_type'].unique())))
+# set title
+ax.set_title('Boxplot of Flat Resale Price by Town')
+# show plot
+st.pyplot(fig)
+
+# set plot and figure size
+fig, ax = plt.subplots(figsize=(15,10))
+# plot boxplot
+sns.boxplot(x='resale_price', y='town', data=data, order=sorted(list(data['town'].unique())))
+# set title
+ax.set_title('Boxplot of Flat Resale Price by Town')
+# show ploy
+st.pyplot(fig)
+
+pip3 freeze >> C:\Users\Russ\anaconda3\Russ Projects\Resale Flat Prices\streamlit_resale_flat_prices\requirements.txt
